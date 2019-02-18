ef18ac515c28bc4043346d69d843ac372899ab41
Joshua <jperrett256@gmail.com>
2019-02-18 23:12:56 +0000

    Can apply saved scroll positions (& fixed styling)
    Added scroll within popup, and made it possible to apply saved scroll
    positions to pages.

--- popup/bookmark.js
+++ popup/bookmark.js
@@ -1,13 +1,14 @@
 $(function() {
+
+	let $list = $('.bookmark-list');
+
 	// TODO remove debug
 
 	/* Construct initial list */
 	browser.storage.local.get('list').then(result => result.list).then(function(list) {
-		// console.log(JSON.stringify(list));
 		if (list) {
 			Object.keys(list).forEach(function(id) {
-				// console.log(JSON.parse(list[id]));
-				$('.bookmark-list').append(generateListItem(id, JSON.parse(list[id])));
+				addListItem(id, JSON.parse(list[id]));
 			});
 		}
 	}).catch(console.error.bind(console));
@@ -17,34 +18,43 @@ $(function() {
 		browser.tabs.executeScript({
 			code: 'window.pageYOffset'
 		}).then(result => result[0]).then(function(scrollY) {
-			// console.log(`scrollY: ${scrollY}`);
 			let promises = [
 				browser.storage.local.get('list').then(result => result && result.list || {}),
 				browser.tabs.query({currentWindow: true, active: true}).then(tab => tab[0].url)
 			];
+
 			return Promise.all(promises).then(function(results) {
 				let [list, url] = results;
 				let id = Date.now();
-				// console.log(`id: ${id}, url: ${url}`);
 				let newItem = {
 					url: url,
 					position: scrollY
 				};
 				list[id] = JSON.stringify(newItem);
-				// console.log(`list: ${JSON.stringify(list)}`);
+
 				return browser.storage.local.set({ list: list }).then(function() {
-					$('.bookmark-list').append(generateListItem(id, newItem));
+					addListItem(id, newItem);
 				});
 			});
 		}).catch(console.error.bind(console));
 	});
 
 	/* Function for adding items to display */
-	function generateListItem(id, item) {
-		return (
+	function addListItem(id, item) {
+		let $new = $(
 			`<li id=${id} class="bookmark-item">` +
 				`<div class="site">${item.url}</div>` +
 			'</li>'
-		);
+		).appendTo($list);
+
+		/* Handle applying scroll positions again */
+		$new.click(function() {
+			browser.storage.local.get('list').then(result => result.list).then(function(list) {
+				return browser.tabs.executeScript({
+					code: `window.scroll({top: ${JSON.parse(list[id]).position}, behavior: 'smooth'})`
+				});
+			}).catch(console.error.bind(console));
+		});
 	}
+
 });
