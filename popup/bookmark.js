5c7e7b198f86aeea41a3fd789f8bc4816bf782a0
Joshua <jperrett256@gmail.com>
2019-02-19 22:56:17 +0000

    Made it possible to remove items
    Also cleaned code.

--- popup/bookmark.js
+++ popup/bookmark.js
@@ -2,8 +2,6 @@ $(function() {
 
 	let $list = $('.bookmark-list');
 
-	// TODO remove debug
-
 	/* Construct initial list */
 	browser.storage.local.get('list').then(result => result.list).then(function(list) {
 		if (list) {
@@ -15,26 +13,24 @@ $(function() {
 
 	/* Handle adding items to list */
 	$('.add-btn').click(function() {
-		browser.tabs.executeScript({
-			code: 'window.pageYOffset'
-		}).then(result => result[0]).then(function(scrollY) {
-			let promises = [
-				browser.storage.local.get('list').then(result => result && result.list || {}),
-				browser.tabs.query({currentWindow: true, active: true}).then(tab => tab[0].url)
-			];
+		let promises = [
+			browser.tabs.executeScript({ code: 'window.pageYOffset' }).then(result => result[0]),
+			browser.storage.local.get('list').then(result => result && result.list || {}),
+			browser.tabs.query({currentWindow: true, active: true}).then(tab => tab[0])
+		];
 
-			return Promise.all(promises).then(function(results) {
-				let [list, url] = results;
-				let id = Date.now();
-				let newItem = {
-					url: url,
-					position: scrollY
-				};
-				list[id] = JSON.stringify(newItem);
+		Promise.all(promises).then(function(results) {
+			let [position, list, tab] = results;
+			let id = Date.now();
+			let newItem = {
+				title: tab.title,
+				url: tab.url,
+				position: position
+			};
+			list[id] = JSON.stringify(newItem);
 
-				return browser.storage.local.set({ list: list }).then(function() {
-					addListItem(id, newItem);
-				});
+			return browser.storage.local.set({ list: list }).then(function() {
+				addListItem(id, newItem);
 			});
 		}).catch(console.error.bind(console));
 	});
@@ -43,7 +39,13 @@ $(function() {
 	function addListItem(id, item) {
 		let $new = $(
 			`<li id=${id} class="bookmark-item">` +
-				`<div class="site">${item.url}</div>` +
+				'<div class="details">' +
+					`<div class="title">${item.title}</div>` +
+					`<div class="url">${item.url}</div>` +
+				'</div>' +
+				'<div class="remove-btn">' +
+					'<object data="icons/x.svg"></object>' +
+				'</div>' +
 			'</li>'
 		).appendTo($list);
 
@@ -53,6 +55,16 @@ $(function() {
 				return browser.tabs.executeScript({
 					code: `window.scroll({top: ${JSON.parse(list[id]).position}, behavior: 'smooth'})`
 				});
+			}).catch(console.error.bind(console));
+		});
+
+		$new.find('.remove-btn').click(function(event) {
+			event.stopPropagation();
+			browser.storage.local.get('list').then(result => result.list).then(function(list) {
+				delete list[id];
+				return browser.storage.local.set({ list: list });
+			}).then(function() {
+				$new.remove();
 			}).catch(console.error.bind(console));
 		});
 	}
