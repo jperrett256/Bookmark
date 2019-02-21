5cd33a2377706cab1126f19f30876ed2bb801b8e
Joshua <jperrett256@gmail.com>
2019-02-21 01:07:05 +0000

    Initially only displays items for active tab
    Items that are not for active tab open a new window, added filter button
    that determines whether all items are displayed or just the items that
    are applicable to the active tab.

--- popup/bookmark.js
+++ popup/bookmark.js
@@ -3,20 +3,41 @@ $(function() {
 	let $list = $('.bookmark-list');
 
 	/* Construct initial list */
-	browser.storage.local.get('list').then(result => result.list).then(function(list) {
-		if (list) {
-			Object.keys(list).forEach(function(id) {
-				addListItem(id, JSON.parse(list[id]));
-			});
-		}
-	}).catch(console.error.bind(console));
+	createList();
+
+	/* Function for creating list */
+	function createList() {
+		let promises = [
+			browser.storage.local.get('list').then(result => result.list),
+			browser.tabs.query({currentWindow: true, active: true}).then(tabs => tabs[0])
+		];
+
+		Promise.all(promises).then(function(results) {
+			let [list, tab] = results;
+			if (list) {
+				Object.keys(list).forEach(function(id) {
+					let item = JSON.parse(list[id]);
+					// TODO need a better way of checking that urls are equal (e.g. may not need complete string match)
+					/* If the filter is on, need to check if the item url matches the tab url */
+					if (item.url === tab.url || !$('.filter-btn').hasClass('active')) addListItem(id, item);
+				});
+			}
+		}).catch(console.error.bind(console));
+	}
+
+	/* Handle toggling filtering (show all or show those for active tab) */
+	$('.filter-btn').click(function() {
+		$(this).toggleClass('active');
+		$('.bookmark-item').remove();
+		createList();
+	});
 
 	/* Handle adding items to list */
 	$('.add-btn').click(function() {
 		let promises = [
 			browser.tabs.executeScript({ code: 'window.pageYOffset' }).then(result => result[0]),
 			browser.storage.local.get('list').then(result => result && result.list || {}),
-			browser.tabs.query({currentWindow: true, active: true}).then(tab => tab[0])
+			browser.tabs.query({currentWindow: true, active: true}).then(tabs => tabs[0])
 		];
 
 		Promise.all(promises).then(function(results) {
@@ -51,10 +72,12 @@ $(function() {
 
 		/* Handle applying scroll positions again */
 		$new.click(function() {
-			browser.storage.local.get('list').then(result => result.list).then(function(list) {
-				return browser.tabs.executeScript({
-					code: `window.scroll({top: ${JSON.parse(list[id]).position}, behavior: 'smooth'})`
+			browser.tabs.query({currentWindow: true, active: true}).then(tabs => tabs[0]).then(function(tab) {
+				if (item.url === tab.url) return browser.tabs.executeScript({
+					code: `window.scroll({top: ${item.position}, behavior: 'smooth'})`
 				});
+				/* If item url is not the url of the active tab, should open items in new tab */
+				return browser.tabs.create({ url: item.url }).then(() => window.close()); // closes window because of 'connection' issue (resetting is a workaround)
 			}).catch(console.error.bind(console));
 		});
 
